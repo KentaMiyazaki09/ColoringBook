a9af803b2bbd23018eaeb7f7a9b114e7e95e3275
Kenta Miyazaki <0924wolf@gmail.com>
2024-03-10 19:25:20 +0900

    Add contents

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,38 @@
 import cat from './cat'
 
-const Doc = document
-function DrawingCat (){
-  // 色の取得
-  const catColors = {
-    eyeLeft: Doc.getElementById('canvas-eye-left').value,
-    eyeRight: Doc.getElementById('canvas-eye-right').value,
-    pattern: Doc.getElementById('canvas-pattern').value
-  }
-
-  // canvasに入れるSVG
-  const catSVG = cat(catColors)
-
-  // canvasにsvgを書き出す
-  const Img = new Image()
-  Img.src = `data:image/svg+xml;base64, ${btoa(catSVG)}`
-  Img.onload = function() {
-    const c = document.getElementById('canvas')
-    const ctx = c.getContext('2d')
-    ctx.drawImage(Img, 0, 0, 666, 666)
-  }
-}
-
 window.addEventListener('DOMContentLoaded', () => {
+  const Doc = document
+
+  function DrawingCat (){
+    // 色の取得
+    const catColors = {
+      eyeLeft: Doc.getElementById('eye-left').value,
+      eyeRight: Doc.getElementById('eye-right').value,
+      patternBack: Doc.getElementById('pattern-back').value,
+      patternTail: Doc.getElementById('pattern-tail').value,
+      patternFace: Doc.getElementById('pattern-face').value,
+      patternArm: Doc.getElementById('pattern-arm').value,
+      patternLeg: Doc.getElementById('pattern-leg').value,
+    }
+
+    // canvasに入れるSVG
+    const catSVG = cat(catColors)
+
+    // canvasにsvgを書き出す
+    const Img = new Image()
+    Img.src = `data:image/svg+xml;base64, ${btoa(catSVG)}`
+    Img.onload = function() {
+      const canvas = document.getElementById('canvas')
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(Img, 0, 0, 666, 666)
+    }
+  }
+
   DrawingCat()
   Doc.getElementById('palette').addEventListener('change', DrawingCat)
+
+  // ダウンロードボタン
+  Doc.getElementById('download-btn').addEventListener('click', function() {
+    this.href = document.getElementById('canvas').toDataURL('image/png')
+  })
 })

8b39f71f632ac43af8d10701fb7a599c18febfd1
Kenta Miyazaki <0924wolf@gmail.com>
2024-03-11 12:38:49 +0900

    Add prettier

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,72 @@
-import cat from './cat'
-import moon from './moon'
-import hat from './hat'
+import cat from "./cat";
+import moon from "./moon";
+import hat from "./hat";
 
-window.addEventListener('DOMContentLoaded', () => {
-  const Doc = document
-  const canvas = document.getElementById('canvas')
-  const ctx = canvas.getContext('2d')
+window.addEventListener("DOMContentLoaded", () => {
+  const Doc = document;
 
-  ctx.beginPath()
-  ctx.fillStyle = 'rgb(255, 255, 255)'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  // 猫を描画するcanvas
+  const canvas = Doc.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-  function DrawingCat (){
+  ctx.beginPath();
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // その他を描画するcanvas
+  const canvasItems = Doc.getElementById("canvas-items");
+  const ctxItems = canvasItems.getContext("2d");
+
+  function DrawingCat() {
     // 色の取得
     const catColors = {
-      eyeLeft: Doc.getElementById('eye-left').value,
-      eyeRight: Doc.getElementById('eye-right').value,
-      patternBack: Doc.getElementById('pattern-back').value,
-      patternTail: Doc.getElementById('pattern-tail').value,
-      patternFace: Doc.getElementById('pattern-face').value,
-      patternArm: Doc.getElementById('pattern-arm').value,
-      patternLeg: Doc.getElementById('pattern-leg').value,
-    }
-    const catSVG = cat(catColors)
+      eyeLeft: Doc.getElementById("eye-left").value,
+      eyeRight: Doc.getElementById("eye-right").value,
+      patternBack: Doc.getElementById("pattern-back").value,
+      patternTail: Doc.getElementById("pattern-tail").value,
+      patternFace: Doc.getElementById("pattern-face").value,
+      patternArm: Doc.getElementById("pattern-arm").value,
+      patternLeg: Doc.getElementById("pattern-leg").value,
+    };
+    const catSVG = cat(catColors);
     // canvasにsvgを書き出す
-    const Img = new Image()
-    Img.src = `data:image/svg+xml;base64, ${btoa(catSVG)}`
+    const Img = new Image();
+    Img.src = `data:image/svg+xml;base64, ${btoa(catSVG)}`;
     Img.onload = () => {
-      ctx.drawImage(Img, 0, 0, 666, 666)
-    }
+      ctx.drawImage(Img, 0, 0, 666, 666);
+    };
 
     // スタンプ描画
     const stamps = {
-      moon: Doc.getElementById('stamp-moon').checked,
-      hat: Doc.getElementById('stamp-hat').checked,
-    }
+      moon: Doc.getElementById("stamp-moon").checked,
+      hat: Doc.getElementById("stamp-hat").checked,
+    };
 
-    if(stamps.hat) {
-      const hatSVG = hat({size: 512})
-      const imgHat = new Image()
-      imgHat.src = `data:image/svg+xml;base64, ${btoa(hatSVG)}`
+    if (stamps.hat) {
+      const hatSVG = hat({ size: 512 });
+      const imgHat = new Image();
+      imgHat.src = `data:image/svg+xml;base64, ${btoa(hatSVG)}`;
       imgHat.onload = () => {
-        ctx.drawImage(imgHat, 105, -40, 200, 200)
-      }
+        ctxItems.drawImage(imgHat, 105, -40, 200, 200);
+      };
     }
 
     if (stamps.moon) {
-      const moonSVG = moon({ size: 512 })
-      const imgMoon = new Image()
-      imgMoon.src = `data:image/svg+xml;base64, ${btoa(moonSVG)}`
+      const moonSVG = moon({ size: 512 });
+      const imgMoon = new Image();
+      imgMoon.src = `data:image/svg+xml;base64, ${btoa(moonSVG)}`;
       imgMoon.onload = () => {
-        ctx.drawImage(imgMoon, 400, 0, 200, 200)
-      }
+        ctxItems.drawImage(imgMoon, 400, 0, 200, 200);
+      };
     }
   }
 
-  DrawingCat()
-  Doc.getElementById('palette').addEventListener('change', DrawingCat)
-  Doc.getElementById('stamp').addEventListener('change', DrawingCat)
+  DrawingCat();
+  Doc.getElementById("palette").addEventListener("change", DrawingCat);
+  Doc.getElementById("stamp").addEventListener("change", DrawingCat);
 
   // ダウンロードボタン
-  Doc.getElementById('download-btn').addEventListener('click', function() {
-    this.href = canvas.toDataURL('image/png')
-  })
-})
+  Doc.getElementById("download-btn").addEventListener("click", function () {
+    this.href = canvas.toDataURL("image/png");
+  });
+});

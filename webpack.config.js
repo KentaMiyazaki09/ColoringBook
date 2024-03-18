368be21adfd2bdf57e84737d26f71dedcc34f9cc
Kenta Miyazaki <0924wolf@gmail.com>
2024-03-18 15:48:44 +0900

    Fix webpack option
    build時にLICENSE.txtを出力しない

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,27 @@
 /** @type {import('webpack').Configuration} */
 
 const { resolve } = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 // development モードか否か？
-const isDev = process.env.NODE_ENV === "development"
+const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
-  mode: isDev ? "development" : "production",
-  devtool: isDev ? "source-map" : undefined,
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'source-map' : undefined,
   entry: {
-    main: './src/js/main.js',
+    main: './src/js/main.js'
   },
   output: {
     path: resolve(__dirname, 'dist/js'),
     publicPath: '/js/',
-    filename: 'main.js',
+    filename: 'main.js'
   },
   devServer: {
     static: {
-      directory: resolve(__dirname, "dist/"),
+      directory: resolve(__dirname, 'dist/')
     },
-    open: true,
+    open: true
   },
   module: {
     rules: [
@@ -29,11 +30,25 @@ const config = {
         // 拡張子 js のファイル（正規表現）
         test: /\.js$/,
         // ローダーの指定
-        loader: "babel-loader",
-      },
-    ],
+        loader: 'babel-loader'
+      }
+    ]
   },
+  optimization: {
+    minimize: true,
+    // build時にLICENSE.txtを出力しない
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false
+          }
+        },
+        extractComments: false
+      })
+    ]
+  }
 }
 
 // 設定を CommonJS 形式でエクスポート
-module.exports = config;
+module.exports = config
